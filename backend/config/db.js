383f81b98f65540d8e9536c7d963c3a87a9cf104
malleshntr <malleshntr33@gmail.com>
2026-07-04 07:17:10 +0530

    Fix production start script

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,8 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect("mongodb://localhost:27017/DarshanEase", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .then(() => console.log("✅ Database connected"))
-// .catch((err) => console.log("❌ DB Connection Failed:", err));
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Database connected"))
-  .catch((err) => console.log("❌ DB Connection Failed:", err));
+mongoose.connect("mongodb://127.0.0.1:27017/DarshanEase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("✅ Database connected"))
+.catch((err) => console.log("❌ DB Connection Failed:", err));
